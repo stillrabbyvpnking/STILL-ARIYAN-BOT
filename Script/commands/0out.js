036b1bda7670cef10898c7cad9447b66d93ee484
stillrabbyvpnking <mdnobinsarker12@gmail.com>
2025-08-30 01:19:29 +0600

    Update 0out.js

--- Script/commands/0out.js
+++ Script/commands/0out.js
@@ -5,7 +5,7 @@ module.exports.config = {
     credits: "𝐂𝐘𝐁𝐄𝐑 ☢️_𖣘 -𝐁𝐎𝐓 ⚠️ 𝑻𝑬𝑨𝑴_ ☢️",
     description: "",
     commandCategory: "Admin",
-    usages: "out [id]",
+    usages: "out [61577864774106] [61566385765901]",
     cooldowns: 10,
 };
 
